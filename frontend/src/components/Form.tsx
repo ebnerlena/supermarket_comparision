7165b33e4714128e7c9fa9bc2408fbdd605a024c
simon <soelder@hotmail.com>
2021-06-03 12:52:16 +0200

    Add header and form to frontend

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,55 @@
+import React, { FormEvent } from "react"
+import { useState } from "react"
+import Switch from "react-switch"
+import styles from "../assets/Form.module.scss"
+
+const Form: React.FC = () => {
+  const [searchText, setSearchText] = useState("")
+  const [priceRange, setPriceRange] = useState("")
+  const [supermarket, setSupermarket] = useState("")
+  const [switchStatus, setSwitchStatus] = useState(false)
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    console.log(searchText)
+    console.log(priceRange)
+    console.log(supermarket)
+  }
+
+  const handleSwitch = (checked: boolean) => {
+    setSwitchStatus(checked)
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.normalWrapper}>
+        <input
+          type="text"
+          name="searchfield"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <input type="submit" name="submit" value="SEARCH" />
+        <label>
+          <span>Advanced Options</span>
+          <Switch onChange={handleSwitch} checked={switchStatus} />
+        </label>
+      </div>
+      {switchStatus ? (
+        <div className={styles.advancedWrapper}>
+          <select
+            name="supermarket"
+            onChange={(e) => setSupermarket(e.target.value)}
+          >
+            <option value="lidl">Lidl</option>
+            <option value="billa">Billa</option>
+          </select>
+        </div>
+      ) : (
+        <></>
+      )}
+    </form>
+  )
+}
+
+export default Form
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "../assets/Header.module.scss"
+
+const Header: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>SUPERMARKET COMPARISON</h1>
+    </div>
+  )
+}
+
+export default Header
