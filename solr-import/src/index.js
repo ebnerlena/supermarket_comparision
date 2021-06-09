42a29c54288b8a291ae163de7f66306022dc563f
fhs44429 <lebner.mmt-b2019@fh-salzburg.ac.at>
2021-06-09 18:05:08 +0200

    changed quantity import field

--- solr-import/src/index.js
+++ solr-import/src/index.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import products from "./../../scrapen/public/products.json";
 
 export const TITLE_FIELD = "title_t_sort";
-export const QUANTITY_FIELD = "quantity_txt_de";
+export const QUANTITY_FIELD = "quantity_t_de";
 export const PRICE_FIELD = "price_f";
 export const IMAGE_FIELD = "image_url_t";
 export const SUPERMARKET_FIELD = "supermarket_t_sort";
@@ -10,20 +10,20 @@ export const SUPERMARKET_FIELD = "supermarket_t_sort";
 const SOLR_URL = "https://vm-cyan.multimediatechnology.at/solr/products";
 
 const buildDocument = (product) => {
-    const document = {};
-    document[TITLE_FIELD] = product.title;
-    document[QUANTITY_FIELD] = product.quantity;
-    document[PRICE_FIELD] = product.price;
-    document[IMAGE_FIELD] = product.image;
-  
-    return document;
-  }
+  const document = {};
+  document[TITLE_FIELD] = product.title;
+  document[QUANTITY_FIELD] = product.quantity;
+  document[PRICE_FIELD] = product.price;
+  document[IMAGE_FIELD] = product.image;
+
+  return document;
+};
 
 const commit = async () => {
   await postSolrRequest("update?commit=true");
-}
+};
 
-const postSolrRequest = async(url, body) => {
+const postSolrRequest = async (url, body) => {
   const jsonResponse = await fetch(`${SOLR_URL}/${url}`, {
     method: "POST",
     body: JSON.stringify(body),
@@ -36,27 +36,26 @@ const postSolrRequest = async(url, body) => {
 
   const response = await jsonResponse.json();
   return response.response;
-}
+};
 
-const deleteAll = async() => {
+const deleteAll = async () => {
   await postSolrRequest("update?commit=true", {
-      delete: {
-          query: "*:*",
-      },
+    delete: {
+      query: "*:*",
+    },
   });
-}
+};
 
 const addDocument = async (fields) => {
   await postSolrRequest("update?overwrite=true&commitWithin=1000", [
     { ...fields },
   ]);
-}
+};
 
 (async () => {
   await deleteAll();
 
-  for(let i = 0; i < products.length; i++)
-  {
+  for (let i = 0; i < products.length; i++) {
     await addDocument(buildDocument(products[i]));
   }
 
